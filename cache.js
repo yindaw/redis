a1b2485dcf2acbc3432ae53165ff03767f16b196
jieGit <2857705371@qq.com>
2020-12-17 18:30:45 +0800

    04

---
+++ cache.js
@@ -0,0 +1,44 @@
+const redis = require("redis");
+const client = redis.createClient();
+const { promisify } = require("util");
+
+const getAsync = promisify(client.get).bind(client);
+const setAsync = promisify(client.set).bind(client);
+
+module.exports = function (options = {}) {
+    const isJSON = options.isJSON == undefined ? true : options.isJSON;
+    return async function (req, res, next) {
+        const key = req.originalUrl;
+        const content = await getAsync(key);
+        if (content) {
+            //有缓存
+            console.log(key, "使用了缓存");
+            const body = isJSON ? JSON.parse(content) : content;
+            res.send(body);
+        } else {
+            //没有缓存
+            //如何获取后续响应中的响应体
+            //express写响应体，最终是通过res.write函数完成
+            const defaultWrite = res.write.bind(res);
+            defaultEnd = res.end.bind(res);
+            const chunks = [];
+            res.write = function (chunk, ...args) {
+                chunks.push(chunk);
+                defaultWrite(chunk, ...args);
+            };
+            res.end = function (chunk, ...args) {
+                if (chunk) {
+                    chunk.push(chunk);
+                }
+                const body = chunks((c) => c.toString("utf-8")).join();
+                if (ttl < 0) { 
+                    setAsync(key, body);
+                } else {
+                    setAsync(key, body, "EX", ttl); 
+                }
+                defaultEnd(chunk, ...args);
+            }
+            next();  
+        }
+    } 
+}
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-const redis = require("redis");
-const { promisify } = require("util");
-const client = redis.createClient();
-// client.set("name", "chengge", (err, reply) => {
-//     console.log(reply);
-// });
+const express = require("express");
+const app = express();
+app.use(express.static("./public"));
 
-const getAsync = promisify(client.get).bind(client);
-
-getAsync("name").then(reply => {
-    console.log(reply);
+const router = express.Router();
+router.get("/:id", require("./cache")({ ttl: 10 }), (req, res) => {
+    console.log(req.originalUrl, "没有使用缓存");
+    //从数据库中取出对应id的新闻数据
+    res.send({
+        title: "新闻标题" + req.params.id,
+        content: "新闻内容" + req.params.id
+    });
 });
+app.use("/api/news", router);
+app.listen(9527); 
